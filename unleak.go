e8445be576efbb3fc2c1682778573aeab5ed92d1
AlasdairF <AlasdairF@users.noreply.github.com>
2014-12-11 15:47:34 +0700

    Now supports all common types + string

--- unleak.go
+++ unleak.go
@@ -12,8 +12,38 @@ func Runes(b []rune) []rune {
 	return v
 }
 
+func String(b string) string {
+	v := make([]byte, len(b))
+	copy(v, b)
+	return string(v)
+}
+
 func Ints(b []int) []int {
 	v := make([]int, len(b))
+	copy(v, b)
+	return v
+}
+
+func Int8s(b []int) []int8 {
+	v := make([]int8, len(b))
+	copy(v, b)
+	return v
+}
+
+func Int16s(b []int) []int16 {
+	v := make([]int16, len(b))
+	copy(v, b)
+	return v
+}
+
+func Int32s(b []int) []int32 {
+	v := make([]int32, len(b))
+	copy(v, b)
+	return v
+}
+
+func Int64s(b []int) []int64 {
+	v := make([]int64, len(b))
 	copy(v, b)
 	return v
 }
@@ -42,8 +72,26 @@ func Uint32s(b []uint32) []uint32 {
 	return v
 }
 
-func Floats(b []float64) []float64 {
+func Uint64s(b []uint64) []uint64 {
+	v := make([]uint64, len(b))
+	copy(v, b)
+	return v
+}
+
+func Float32s(b []float32) []float32 {
+	v := make([]float32, len(b))
+	copy(v, b)
+	return v
+}
+
+func Float64s(b []float64) []float64 {
 	v := make([]float64, len(b))
+	copy(v, b)
+	return v
+}
+
+func Bools(b []bool) []bool {
+	v := make([]bool, len(b))
 	copy(v, b)
 	return v
 }
